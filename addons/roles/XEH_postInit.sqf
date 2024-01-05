5593cae4b2bf26f253d4ae3b5a2d58cb23a73189
Freya Gaynor <freya@webwitch.org>
2024-01-05 11:23:56 -0800

    refactor: Format a few files

--- addons/roles/XEH_postInit.sqf
+++ addons/roles/XEH_postInit.sqf
@@ -5,7 +5,7 @@ if (!hasInterface) exitWith {};
 private _playerRole = player getVariable QGVARMAIN(role);
 
 if (!isNil "_playerRole") exitWith {
-    TRACE_1("Player has a role already, exiting",_playerRole);
+  TRACE_1("Player has a role already, exiting",_playerRole);
 };
 
 [player, QUOTE(DEFAULT_ROLE)] call EFUNC(common,setUnitRole);
--- addons/server/config.cpp
+++ addons/server/config.cpp
@@ -4,17 +4,17 @@
 
 class CfgPatches
 {
-    class ADDON
-    {
-        name = QUOTE(COMPONENT);
-        units[] = { };
-        weapons[] = {};
-        requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = { "erp_arsenal", "erp_roles", "erp_db", "inidbi2" };
-        skipWhenMissingDependencies = 1;
-        author = QUOTE(AUTHOR);
-        VERSION_CONFIG;
-    };
+  class ADDON
+  {
+    name = QUOTE(COMPONENT);
+    units[] = { };
+    weapons[] = {};
+    requiredVersion = REQUIRED_VERSION;
+    requiredAddons[] = { "erp_arsenal", "erp_roles", "erp_db", "inidbi2" };
+    skipWhenMissingDependencies = 1;
+    author = QUOTE(AUTHOR);
+    VERSION_CONFIG;
+  };
 };
 
 
