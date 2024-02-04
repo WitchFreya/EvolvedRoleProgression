340a017ae51ffce7e6c7937b93ec47b949ad45f0
Freya Gaynor <freya@webwitch.org>
2024-02-04 12:11:40 -0800

    refactor(db): Remove assert_db

--- addons/db/script_component.hpp
+++ addons/db/script_component.hpp
@@ -13,9 +13,3 @@
 #endif
 
 #include "\x\erp\addons\main\script_macros.hpp"
-
-#define ASSERT_DB(db,msg,varExit) ASSERT_FALSE_EXIT(isNil {"getVersion" call db},msg,varExit)
-
-// nil sucks
-#define NOTHING "$NOTHING"
-#define IS_NOTHING(var0) IS_STRING(var0) && {var0 == NOTHING}
