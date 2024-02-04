f60a2a0d8de6c6939cd937f949815a10beba2f22
Freya Gaynor <freya@webwitch.org>
2024-02-04 10:12:43 -0800

    refactor(main): Script macros format

--- addons/main/script_macros.hpp
+++ addons/main/script_macros.hpp
@@ -3,25 +3,25 @@
 #define DFUNC(var1) TRIPLES(ADDON,fnc,var1)
 
 #ifdef DISABLE_COMPILE_CACHE
-    #undef PREP
-    #define PREP(fncName) DFUNC(fncName) = compile preprocessFileLineNumbers QPATHTOF(functions\DOUBLES(fnc,fncName).sqf)
+  #undef PREP
+  #define PREP(fncName) DFUNC(fncName) = compile preprocessFileLineNumbers QPATHTOF(functions\DOUBLES(fnc,fncName).sqf)
 #else
-    #undef PREP
-    #define PREP(fncName) [QPATHTOF(functions\DOUBLES(fnc,fncName).sqf), QFUNC(fncName)] call CBA_fnc_compileFunction
+  #undef PREP
+  #define PREP(fncName) [QPATHTOF(functions\DOUBLES(fnc,fncName).sqf), QFUNC(fncName)] call CBA_fnc_compileFunction
 #endif
 
-#define SCOPE(var0)      \
-    scope = var0;        \
-    scopeCurator = var0; \
-    scopeArsenal = var0
+#define SCOPE(var0)     \
+  scope = var0;         \
+  scopeCurator = var0;  \
+  scopeArsenal = var0
 
-#define META                \
-    dlc = QUOTE(PREFIX);    \
-    author = QUOTE(AUTHOR)
+#define META            \
+  dlc = QUOTE(PREFIX);  \
+  author = QUOTE(AUTHOR)
 
 #define ITEM_META(var0) \
-    META;                   \
-    SCOPE(var0)
+  META;                 \
+  SCOPE(var0)
 
 #define DEFAULT_ROLE Rifleman
 #define BASE_ROLE Default
